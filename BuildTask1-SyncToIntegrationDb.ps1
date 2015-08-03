48bb3bae056c8d006e8869afbd47289d35731043
Alex-Yates <ajy.mail@gmail.com>
2015-08-03 18:22:30 +0100

    Improving comments

--- BuildTask1-SyncToIntegrationDb.ps1
+++ BuildTask1-SyncToIntegrationDb.ps1
@@ -1,9 +1,12 @@
+# Configuration
 $schemaComparePath = 'C:\\Program Files\\Red Gate\\Schema Compare for Oracle 3\\SCO.exe'
 $targetSchemaName = "${bamboo.OracleDbIntegrationSchema}"
 $host = "${bamboo.OracleDbHost}"
 $username = "${bamboo.OracleDbUser}"
 $password = "${bamboo.OracleDbPassword}"
 $sourceName = "${bamboo.SourceSchemaName}"
+
+# Logging config variables for troubleshooting
 Write-Output "Using the following variables:"
 Write-Output "schemaComparePath: $schemaComparePath"
 Write-Output "targetSchemaName: $targetSchemaName"
@@ -12,12 +15,14 @@ Write-Output "username: $username"
 Write-Output "password: has been set to the bamboo variable OracleDbPassword"
 Write-Output "sourceName: $sourceName"
 
+# Deploy changes using Redgate Schema Compare for Oracle
 Write-Warning "Upgrading $targetSchemaName"
 
-# Create upgrade script using compare
 & $schemaComparePath -source "${bamboo.build.working.directory}\Widget\db\state{$sourceName}" -target "$username/$password@$host{$targetSchemaName}" -deploy -scriptfile "${bamboo.build.working.directory}\artifacts\upgradeScript.sql" -report  "${bamboo.build.working.directory}\artifacts\diffReport.html" -reporttype Simple -includeidentical | Out-Host
 
+# Logging the Schema Compare exit code and path to artifacts
 Write-Output "Schema Compare for Oracle exited with code $lastExitCode"
+Write-Output "UpgradeScript and diff report created and saved to ${bamboo.build.working.directory}\artifacts"
 
 # Exit code 61 is simply telling us there are differences that have been deployed.
 if( $lastExitCode -eq 61)
