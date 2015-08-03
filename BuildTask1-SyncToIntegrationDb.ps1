05e566a5d5e4ae83594340bb7c26f9b545cfffd3
Alex-Yates <ajy.mail@gmail.com>
2015-08-03 18:13:17 +0100

    Renaming Test > Integration. Parameterising $host.

--- BuildTask1-SyncToIntegrationDb.ps1
+++ BuildTask1-SyncToIntegrationDb.ps1
@@ -1,11 +1,13 @@
 $schemaComparePath = 'C:\\Program Files\\Red Gate\\Schema Compare for Oracle 3\\SCO.exe'
-$targetSchemaName = "${bamboo.OracleDbTestSchema}"
+$targetSchemaName = "${bamboo.OracleDbIntegrationSchema}"
+$host = "${bamboo.OracleDbHost}"
 $username = "${bamboo.OracleDbUser}"
 $password = "${bamboo.OracleDbPassword}"
 $sourceName = "${bamboo.SourceSchemaName}"
 Write-Output "Using the following variables:"
 Write-Output "schemaComparePath: $schemaComparePath"
 Write-Output "targetSchemaName: $targetSchemaName"
+Write-Output "host: $host"
 Write-Output "username: $username"
 Write-Output "password: has been set to the bamboo variable OracleDbPassword"
 Write-Output "sourceName: $sourceName"
@@ -13,7 +15,7 @@ Write-Output "sourceName: $sourceName"
 Write-Warning "Upgrading $targetSchemaName"
 
 # Create upgrade script using compare
-& $schemaComparePath -source "${bamboo.build.working.directory}\Widget\db\state{$sourceName}" -target "$username/$password@localhost{$targetSchemaName}" -deploy -scriptfile "${bamboo.build.working.directory}\artifacts\upgradeScript.sql" -report  "${bamboo.build.working.directory}\artifacts\diffReport.html" -reporttype Simple -includeidentical | Out-Host
+& $schemaComparePath -source "${bamboo.build.working.directory}\Widget\db\state{$sourceName}" -target "$username/$password@$host{$targetSchemaName}" -deploy -scriptfile "${bamboo.build.working.directory}\artifacts\upgradeScript.sql" -report  "${bamboo.build.working.directory}\artifacts\diffReport.html" -reporttype Simple -includeidentical | Out-Host
 
 Write-Output "Schema Compare for Oracle exited with code $lastExitCode"
 
